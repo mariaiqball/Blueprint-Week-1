bead26e9b61499d3c57fa6f05733d03edd0cb0c6
Arush Verma <63593892+Arush2604@users.noreply.github.com>
2023-10-09 17:29:40 -0400

    Update App.java
    A/C

--- Intro.java/src/App.java
+++ Intro.java/src/App.java
@@ -1,1 +1,2 @@
 //Hi! My name is Arushi! My least favorite app is DUO Mobile. A fun fact about me is that I used to cry everytime I would go to the optometrist as a kid. Something that I am listening to right now is Drake's new album: For all the dogs. 
+//Hi my name is John Carlo! My favorite invention is the air conditioning unit.
