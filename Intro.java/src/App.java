a1248f111de8b33681bb408e1305ac6d2e1c7cde
arushi18p <66833087+arushi18p@users.noreply.github.com>
2023-10-09 20:31:20 -0400

    Update Intro
    N/A

--- Intro.java/src/App.java
+++ Intro.java/src/App.java
@@ -1,3 +1,4 @@
 //Hi! My name is Arushi! My least favorite app is DUO Mobile. A fun fact about me is that I used to cry everytime I would go to the optometrist as a kid. Something that I am listening to right now is Drake's new album: For all the dogs. 
 //Hi my name is John Carlo! My favorite invention is the air conditioning unit.
 //My name is Jane Doe and my favorite color is green and I love Ryan Reynolds. 
+//Hi My name is Arushi and a fun fact about me is that I hate the smell of cheese. 
